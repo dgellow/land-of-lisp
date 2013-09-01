f4cebf37ebd2ca362b5930efd17c53d6c69cc04e
Sam E <georgesleyetidoux@gmail.com>
2013-09-01 13:52:58 +0200

    Add a file header

--- 1_guess_number.lisp
+++ 1_guess_number.lisp
@@ -1,3 +1,10 @@
+;;; 1_guess_number.lisp -  A basic "guess my number" game
+
+;; Copyright (C) 2013 Samuel El-Borai
+
+;; Author: Samuel El-Borai <samuel.elborai@gmail.com>
+;; Created: 1 Sept 2013
+
 (defun play (high-limit)
   (let ((guess (random high-limit))
 	(high high-limit)
